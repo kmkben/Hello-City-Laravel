648371a5724aab8958e7fcebfb98acd70aec140e
Kemoko KEITA <kemokokeita710@gmail.com>
2023-01-08 04:44:14 +0000

    Adding images flag and logo

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,9 @@
 @section('title', 'About Us | ' . config('app.name'))
 
 @section('content')
+
+    <img src="/images/kmkit-logo.jpg" alt="Kmk Logo" />
+
     <h1>Built with &hearts; by Kmk</h1>
 
     <p><a href="{{ route('app_home') }}">Go back </a></p>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,9 @@
 @extends('app')
 
 @section('content')
+
+    <img src="/images/flag-fr.jpg" alt="France flag"/>
+
     <h1>Hello form Paris!</h1>
 
     <p>It's currently {{ date('h:i A') }} in Paris</p>
